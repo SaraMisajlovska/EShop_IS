b735e35252891df2a4ac972b87ca1432da9a7cbc
SaraMisajlovska <smisajlovska@gmail.com>
2022-07-09 17:34:23 +0200

    added importusers functionality

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using EShop.Domain.DomainModels;
+using EShop.Domain.Identity;
 using EShop.Service.Interface;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,21 +15,58 @@ namespace EShop.Controllers
     [ApiController]
     public class AdminController : ControllerBase
     {
-        public readonly IOrderService _orderService;
+        private readonly IOrderService _orderService;
+        private readonly UserManager<ShopApplicationUser> _userManager;
 
-        public AdminController(IOrderService orderService)
+        public AdminController(IOrderService orderService,
+            UserManager<ShopApplicationUser> userManager)
         {
             _orderService = orderService;
+            _userManager = userManager;
         }
+
         [HttpGet("[action]")]
         public List<Order> GetAllActiveOrders()
         {
             return _orderService.GetAllOrders();
         }
+
         [HttpPost("[action]")]
         public Order GetOrderDetails(BaseEntity model)
         {
             return _orderService.GetOrderDetails(model);
+        }
+
+        [HttpPost("[action]")]
+        public bool ImportAllUsers(List<UserRegistrationDto> model)
+        {
+            bool status = true;
+            foreach (var user in model)
+            {
+                var userCheck = _userManager.FindByEmailAsync(user.Email).Result;
+                if (userCheck == null)
+                {
+                    var newUser = new ShopApplicationUser
+                    {
+                        Name = "",
+                        Surname = "",
+                        Address = "",
+                        UserName = user.Email,
+                        NormalizedUserName = user.Email,
+                        Email = user.Email,
+                        UserShoppingCart = new ShoppingCart()
+                    };
+
+                    var result = _userManager.CreateAsync(newUser, user.Password).Result;
+                    status = status && result.Succeeded;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            return status;
         }
     }
 }
--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-
-
 using System.Security.Claims;
-using EShop.Repository;
 using EShop.Domain.DTO;
 using EShop.Domain.DomainModels;
 using EShop.Service.Interface;
+
 
 namespace EShop.Controllers
 {
